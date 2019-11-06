042d2133aa6f98d429a824e1bae1eb6bcd62b123
Nil <45631804+Fedakope@users.noreply.github.com>
2019-11-06 13:55:25 +0100

    Initial commit
    Based on HTML Template repo
    just added some cards and titles

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    $(function(){ $("#navbar").load("components/navbar.html") })  
+    $(function(){ $("#jumbotron").load("components/jumbotron.html") }) 
+    $(function(){ $("#footer").load("components/footer.html") }) 
+    $(function(){ $("#people").load("components/people.html") }) 
+  });
+
+  
