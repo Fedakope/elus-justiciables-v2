cb4c803206b0b2f7e4de247703fef4a4643134ac
Besombes <nil.besombes@esker.corp>
2019-11-07 13:51:58 +0100

    Seeds and InjectRandomFact, pic on local folder for 3 ppl, removed footer

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     $(function(){ $("#navbar").load("components/navbar.html") })  
     $(function(){ $("#jumbotron").load("components/jumbotron.html") }) 
-    $(function(){ $("#footer").load("components/footer.html") }) 
+    // $(function(){ $("#footer").load("components/footer.html") }) 
     $(function(){ $("#people-cards").load("components/people-cards.html") }) 
+    $(function(){ $("#well").load("components/well.html") }) 
   });
+
+  
+  function InjectRandomFact() {
+    $("#well").empty()
+    $("#well").append("<h1>" + RandomFact().Content + "</h1>")
+  }
+   
 
   
--- seeds.js
+++ seeds.js
@@ -1,14 +1,18 @@
 function Seeds() {
-    const balkany = new People("", "Patrick", "Balkany", "", "pic_url" )
+    // cleaning 
+    factList = [];
+    peopleList = [];
+    // seeding
+    const balkany = new People("", "Patrick", "Balkany", "", "/components/img/balkany.jpg" )
     const bal1 = new Fact("", "Blanchiment de fraude fiscale", balkany  )
     const bal2 = new Fact("", "Abus de biens sociaux", balkany  )
     balkany.Facts = [bal1, bal2]
 
-    const sarkozy = new People("", "Nicolas", "Sarkozy", "", "pic_url" )
+    const sarkozy = new People("", "Nicolas", "Sarkozy", "", "/components/img/sarkozy.jpg" )
     const sar1 = new Fact("", "Blanchiment de fraude fiscale", sarkozy  )
     sarkozy.Facts = sar1
 
-    const cahuzac = new People("", "Jerôme", "Cahuzac", "", "pic_url" )
+    const cahuzac = new People("", "Jerôme", "Cahuzac", "", "/components/img/cahuzac.jpg" )
     const cah1 = new Fact("", "Phobie Administrative", cahuzac)
     cahuzac.Facts = cah1
 }
